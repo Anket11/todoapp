6dcf2420bf48a43a4dae81d65ab23f1af000d776
Anket <patilanket11@gmail.com>
2022-06-21 18:56:51 +0530

    Added delete todo feature

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Todo from './Todo'
+import Todo, { handleCheckBox } from "./Todo";
 function App() {
   const [inputText, setInputText] = useState("");
   const [items, setItems] = useState([]);
@@ -8,31 +8,46 @@ function App() {
     const newValue = event.target.value;
     setInputText(newValue);
   }
-
+  function handleEnter(event){
+    if(event.key == "Enter"){
+      event.preventDefault();
+      setInputText(event.target.value);
+      addItem();
+    }
+  }
   function addItem() {
-    setItems(prevItems => {
+    setItems((prevItems) => {
       return [...prevItems, inputText];
     });
     setInputText("");
   }
-
+  
+  const onDelete = (item) => {
+    setItems(
+      items.filter((e) => {
+        return e !== item.content;
+      })
+    );
+    console.log("deleted", item);
+  };
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input onChange={handleChange} type="text" value={inputText} />
+        <input onChange={handleChange} onKeyPress={handleEnter} type="text" value={inputText} />
         <button onClick={addItem}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          {items.map(todoItem => (
-            <Todo content = {todoItem}/>
+          {items.map((todoItem) => (
+            <Todo key={todoItem} content={todoItem} onDelete={onDelete} />
           ))}
         </ul>
+
       </div>
     </div>
   );
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
-
+import React,{useState} from 'react'
 function Todo (props){
+    const [checked, setChecked] = useState(false);
+    const handleCheckBox = event => {
+        if (event.target.checked) {
+            {props.onDelete(props)}
+          console.log('✅ Checkbox is checked');
+          
+          console.log(checked);
+        }
+        setChecked(false);
+      };
   return (
-    <li>{props.content}</li>
+    <li ><input className="form-check-input me-1" type="checkbox" onChange={handleCheckBox} checked={checked} aria-label="..."></input>{props.content}</li>
   )
 }
 export default Todo;
+// export {handleCheckBox};
